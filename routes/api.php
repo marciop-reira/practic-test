2f6b22a1a2596d6d3520cc602417e7a73d12e805
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 11:16:09 -0300

    Add fallback routes

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('store', StoreController::class);
 Route::apiResource('product', ProductController::class);
+
+Route::fallback(function (){
+    return response()->json(['message' => 'Not found'], 404);
+});
