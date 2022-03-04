689013db4ccb73b60a795a15575629542ae8a83e
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 02:22:29 -0300

    Add product repository

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Repositories\Contracts\ProductRepositoryInterface;
+
+/**
+ *
+ */
+class ProductRepository implements ProductRepositoryInterface
+{
+
+    /**
+     * @var Product
+     */
+    protected $entity;
+
+    /**
+     * @param Product $product
+     */
+    public function __construct(Product $product)
+    {
+        $this->entity = $product;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAllProducts()
+    {
+        return $this->entity->paginate();
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function getProductById(int $id)
+    {
+        return $this->entity->findOrFail($id);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function createProduct(array $data)
+    {
+        return $this->entity->create($data);
+    }
+
+    /**
+     * @param object $product
+     * @param array $data
+     * @return mixed
+     */
+    public function updateProduct(object $product, array $data)
+    {
+        return $product->update($data);
+    }
+
+    /**
+     * @param object $product
+     * @return mixed
+     */
+    public function destroyProduct(object $product)
+    {
+        return $product->delete();
+    }
+}
