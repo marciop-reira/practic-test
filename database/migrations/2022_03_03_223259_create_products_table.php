e4dd592bfc140177b0f7dcf9ee8e7e0f6e6acc30
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-03 22:44:05 -0300

    Created migration to products

---
+++ database/migrations/2022_03_03_223259_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->string('name');
+            $table->integer('value');
+            $table->boolean('active');
+            $table->timestamps();
+
+            $table->foreign('store_id')
+                ->on('stores')
+                ->references('id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
