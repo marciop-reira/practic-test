544a7fb0b5afa08dd82a8208bd510cc0cb6ff8f4
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 10:23:32 -0300

    Extend trait notifiable

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
 
 /**
  *
  */
 class Store extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     /**
      * @var string[]
