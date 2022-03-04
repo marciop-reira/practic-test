dcd51fcccfbbed9d7cf94e3127e0d09265a4febc
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 03:19:41 -0300

    Fix product route

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\{
-    StoreController
+    StoreController,
+    ProductController,
 };
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('store', StoreController::class);
+Route::apiResource('product', ProductController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
