102d4ace5bcaf21e5ed5ddeaec312c87aac2075e
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 02:41:46 -0300

    Add product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'store_id' => $this->faker->numberBetween(1, 1),
+            'name' => $this->faker->name,
+            'value' => $this->faker->numberBetween(1, 999999),
+            'active' => $this->faker->boolean,
+        ];
+    }
+}
