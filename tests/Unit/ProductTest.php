96d8ab2246a6368af94c3e830173984a1bcebf06
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 02:37:40 -0300

    Add product test unit

---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function test_check_if_store_fields_are_correct()
+    {
+        $product = new Product();
+        $expected = [
+            'store_id',
+            'name',
+            'value',
+            'active',
+        ];
+        $arrayCompared = array_diff($expected, $product->getFillable());
+
+        $this->assertEmpty($arrayCompared);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_check_if_store_has_relationship_products()
+    {
+        $this->assertTrue(method_exists(Product::class, 'store'));
+    }
+}
