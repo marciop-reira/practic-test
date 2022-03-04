2fc763d881882a905195b82e24d9c54a019b6f2f
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 02:11:54 -0300

    Configure provider to handle dependency injection

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Contracts\StoreRepositoryInterface;
+use App\Repositories\StoreRepository;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ *
+ */
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            StoreRepositoryInterface::class,
+            StoreRepository::class
+        );
+
+//        $this->app->bind(
+//            Produ::class,
+//            ProductRepository::class
+//        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
