aa7e351235bea61923b3130314ab237f4a3d5c9c
Márcio Pereira <marcio.brisanet@gmail.com>
2022-03-04 02:13:16 -0300

    Add unit tests to store

---
+++ tests/Unit/StoreTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Store;
+use PHPUnit\Framework\TestCase;
+
+class StoreTest extends TestCase
+{
+
+    public function test_check_if_store_fields_are_correct()
+    {
+        $store = new Store();
+        $expected = [
+            'name',
+            'email',
+        ];
+        $arrayCompared = array_diff($expected, $store->getFillable());
+
+        $this->assertEmpty($arrayCompared);
+    }
+
+    public function test_check_if_store_has_relationship_products()
+    {
+        $this->assertTrue(method_exists(Store::class, 'products'));
+    }
+}
